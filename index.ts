98618b91d1c1b626257f07c4a4f011898507638b
Noureddine BOUCHENTOUF <noureddine.bouchentouf@holcim.com>
2026-09-24 01:29:49 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { createClient } from "npm:@supabase/supabase-js@2";
 
 const supabase = createClient(
-  Deno.env.get("SUPABASE_URL")!,
+  Deno.env.get("https://dlarqdsxsswdnpnloaks.supabase.co/rest/v1/")!,
   Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!
 );
 
